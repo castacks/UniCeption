3ee6105005e24d1c0173f9e81fe91475ae880d2f
infinity1096 <2315465052@qq.com>
2025-02-26 22:57:40 -0800

    reverted symmetric setting, added unimatch scaler

--- uniception/models/encoders/image_normalizations.py
+++ uniception/models/encoders/image_normalizations.py
@@ -23,5 +23,8 @@ IMAGE_NORMALIZATION_DICT = {
     "sea_raft": ImageNormalization(
         mean=torch.tensor([0.0, 0.0, 0.0]), std=torch.tensor([1.0, 1.0, 1.0]) / 255
     ),  # Sea-RAFT uses 0-255 in FP32
+    "unimatch": ImageNormalization(
+        mean=torch.tensor([0.0, 0.0, 0.0]), std=torch.tensor([1.0, 1.0, 1.0]) / 255
+    ),  # UniMatch uses 0-255 in FP32
     "cosmos": ImageNormalization(mean=torch.tensor([0.0, 0.0, 0.0]), std=torch.tensor([0.5, 0.5, 0.5])),
 }
