410a66cf7e4d8fddb4a5c8be0a2846ea6dc2e608
Nik-V9 <keethanikhil@gmail.com>
2025-06-07 21:14:17 -0400

    Remove Match Anything from Model Factory

--- uniception/models/factory/__init__.py
+++ uniception/models/factory/__init__.py
@@ -1,4 +1,3 @@
 from uniception.models.factory.dust3r import DUSt3R
-from uniception.models.factory.match_anything import MatchAnythingModel
 
-__all__ = ["DUSt3R", "MatchAnythingModel"]
+__all__ = ["DUSt3R"]
